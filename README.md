1c34fd9167cbf9373a4715d28eb3e6171eaddde3
김지영 <kimjiyeong@gimjiyeong-ui-Macmini.local>
2023-07-27 17:26:18 +0900

    01. setup

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # graphQL
+> `package.json`파일에서 `"type" : "module"` 을 넣으면 `import`문으로 js파일에 적용할 수 있다.(`index.js`파일에서 참조)
+
+## 01. setup
+```bash
+Error: Apollo Server requires either an existing schema, modules or typeDefs
+```
---
+++ index.js
@@ -0,0 +1,11 @@
+// package.json에 "type" : "module"을 설정했을 때
+import { ApolloServer, gql } from "apollo-server";
+
+// package.json에 "type" : "module"을 설정하지 않았을 때
+// const {ApolloServer, gql} = require("apollo-server") 
+
+const server = new ApolloServer({})
+
+server.listen().then(({url}) => {
+    console.log(`running on ${url}`)
+})
