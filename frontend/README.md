596efdde3115c1802049c0cecae1497147191ea7
김지영 <kimjiyeong@gimjiyeong-ui-Macmini.local>
2023-07-31 12:43:07 +0900

    5. useQuery

--- frontend/README.md
+++ frontend/README.md
@@ -118,3 +118,41 @@ const Movies = () => {
     }).then((res) => setMovies(res.data.allMovies))
    }, [client])
 ```
+
+## 5. useQuery
+```javascript
+// Movies.jsx
+
+import { gql, useQuery } from '@apollo/client'
+import { Link } from 'react-router-dom'
+
+const ALL_MOVIES = gql`
+  {
+    allMovies {
+      Title
+      imdbID
+    }
+  }
+`
+
+const Movies = () => {
+  const {data, loading, error} = useQuery(ALL_MOVIES)
+
+  if(loading) {
+    return <h1>Loading...</h1>
+  }
+  if(error) {
+    return <h1>Could not fetch :(</h1>
+  }
+
+  return (
+    <div>
+      {data.allMovies.map((item) => (
+        <div key={item.imdbID}>
+          <Link to={`/movies/${item.imdbID}`}>{item.Title}</Link>
+        </div>
+      ))}
+    </div>
+  )
+}
+```
--- frontend/src/routes/Movies.jsx
+++ frontend/src/routes/Movies.jsx
@@ -1,25 +1,32 @@
-import { gql, useApolloClient } from '@apollo/client'
-import React, { useEffect, useState } from 'react'
+import { gql, useQuery } from '@apollo/client'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const ALL_MOVIES = gql`
+  {
+    allMovies {
+      Title
+      imdbID
+    }
+  }
+`
 
 const Movies = () => {
-  const client = useApolloClient()
-  const [Movies, setMovies] = useState([])
-   useEffect(() => {
-    client.query({
-      query : gql`
-      {
-        allMovies {
-          Title
-        }
-      }
-      `
-    }).then((res) => setMovies(res.data.allMovies))
-   }, [client])
-   
+  const {data, loading, error} = useQuery(ALL_MOVIES)
+
+  if(loading) {
+    return <h1>Loading...</h1>
+  }
+  if(error) {
+    return <h1>Could not fetch :(</h1>
+  }
+
   return (
     <div>
-      {Movies.map((item) => (
-        <div key={item.Title}>{item.Title}</div>
+      {data.allMovies.map((item) => (
+        <div key={item.imdbID}>
+          <Link to={`/movies/${item.imdbID}`}>{item.Title}</Link>
+        </div>
       ))}
     </div>
   )
